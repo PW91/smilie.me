692d61b645dfd53cac7679adcc9d1a870ac0f7cd
Przemyslaw Wieteska <wieteska.przemyslaw@gmail.com>
2017-06-19 22:10:35 +0200

    animation & sizing done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
  var myObject = {
 
    init: function() {
 
-     this.playButton = document.querySelector(".play-button-wrapper");
-     this.videoCurtain = document.querySelector(".video-curtain");
-     this.videoCloseButton = this.videoCurtain.querySelector(".close-button-wrapper");
-     this.iframe = this.videoCurtain.querySelector("iframe");
-     this.questionMarkButton = document.querySelector(".header .question-mark-wrapper");
-     this.aboutCurtain = document.querySelector(".about-curtain");
-     this.aboutCloseButton = this.aboutCurtain.querySelector(".close-button-wrapper");
-     this.footer = document.querySelector(".footer");
+     this.playButton = document.querySelector('.play-button-wrapper');
+     this.videoCurtain = document.querySelector('.video-curtain');
+     this.videoCloseButton = this.videoCurtain.querySelector('.close-button-wrapper');
+     this.iframe = this.videoCurtain.querySelector('iframe');
+     this.questionMarkButton = document.querySelector('.header .question-mark-wrapper');
+     this.aboutCurtain = document.querySelector('.about-curtain');
+     this.aboutCloseButton = this.aboutCurtain.querySelector('.close-button-wrapper');
+     this.footer = document.querySelector('.footer');
 
      if (this.playButton && this.videoCloseButton && this.videoCurtain) {
        this.videoFeatures();
@@ -25,27 +25,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
    videoFeatures: function() {
 
-     this.playButton.addEventListener("click", function() {
-       this.videoCurtain.classList.add("visible");
+     this.playButton.addEventListener('click', function() {
+       this.videoCurtain.classList.add('visible');
      }.bind(this))
 
-     this.videoCloseButton.addEventListener("click", function() {
-       this.videoCurtain.classList.remove("visible");
+     this.videoCloseButton.addEventListener('click', function() {
+       this.videoCurtain.classList.remove('visible');
      }.bind(this))
 
    },
 
    aboutFeatures: function() {
 
-     this.questionMarkButton.addEventListener("click", function() {
-       this.aboutCurtain.classList.add("visible");
+     this.questionMarkButton.addEventListener('click', function() {
+       this.aboutCurtain.classList.add('visible');
 
        var footerPosition = this.footer.offsetTop;
-       this.aboutCurtain.style.height = (footerPosition - 20)+ "px";
+       this.aboutCurtain.style.height = (footerPosition - 20)+ 'px';
      }.bind(this))
 
-     this.aboutCloseButton.addEventListener("click", function() {
-       this.aboutCurtain.classList.remove("visible");
+     this.aboutCloseButton.addEventListener('click', function() {
+       this.aboutCurtain.classList.remove('visible');
      }.bind(this))
 
    }
